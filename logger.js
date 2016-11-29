b7421abec31b44b512a8daf209b31fe30ba756ed
mazdik <remappp@gmail.com>
2016-11-29 19:53:18 +0500

    logs

--- logger.js
+++ logger.js
@@ -1,9 +1,16 @@
-'use strict';
+var log4js = require('log4js');
 
-var log4js = require('log4js'); 
-log4js.loadAppender('file');
-log4js.addAppender(log4js.appenders.file('log.log'), 'log');
-var logger = log4js.getLogger('log');
+log4js.configure({
+    appenders: [{
+        type: 'file',
+        filename: "logs/log.log",
+        category: 'log',
+        maxLogSize: 20480,
+        backups: 2
+    }]
+});
+
+let logger = log4js.getLogger('log');
 logger.setLevel('DEBUG');
 
 module.exports = logger;
